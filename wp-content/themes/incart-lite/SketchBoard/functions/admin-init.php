1b3bddaf4273ac20a9991978772f7da79c87711e
AJ Hernandez <ajhernandez@gmail.com>
2019-06-28 18:58:04 +0800

    Initial - Batch 3

---
+++ wp-content/themes/incart-lite/SketchBoard/functions/admin-init.php
@@ -0,0 +1,12 @@
+<?php
+/********************************************
+ FRAME WORK CODE STARTS HERE
+*********************************************/
+/********************************************
+ THEME VARIABLES
+*********************************************/
+global $incart_lite_themename;
+global $incart_lite_shortname;
+require_once(get_template_directory().'/SketchBoard/functions/sketch-functions.php');                // Pagination, excerpt control etc.
+require_once(get_template_directory().'/SketchBoard/functions/sketch-enqueue.php');                  // Enqueue Css Scripts
+require_once(get_template_directory().'/SketchBoard/functions/sketch-breadcrumb.php');               // Include Breadcrumb       // Includes Widget
